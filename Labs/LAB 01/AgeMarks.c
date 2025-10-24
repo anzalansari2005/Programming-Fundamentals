c468ba64e37d90666e8602ded5afc6d12fc6c7db
anzalansari2005 <anzalansari222@gmail.com>
2025-10-24 22:07:04 +0500

    Reupload

---
+++ Labs/LAB 01/AgeMarks.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void){
+    int age = 18;
+    float marks = 89.5;
+    char grade = 'A;
+    printf("Age : %d\nMarks : %.3f\nGrade : %c\n",age,marks,grade);
+}
---
+++ Labs/LAB 01/NameDept.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(void){
+    printf("Name:\tAnzal Ansari\nDepartment:\tCSIT\n");
+    printf("1st semester\n");
+}
