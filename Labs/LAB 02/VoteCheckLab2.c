897af32a0759b8bfb629beadc6a74daeae76b91c
anzalansari2005 <anzalansari222@gmail.com>
2025-10-24 22:17:32 +0500

    Reupload after organising

---
+++ Labs/LAB 02/VoteCheckLab2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+    int age;
+    int hasID;
+    printf("Enter your age: ");
+    scanf("%d",&age);   
+    if(age>=18){
+        printf("Do you have ID(enter 1 if yes 0 if no): ");
+        scanf("%d",&hasID);
+
+        if(age>=18 && hasID==1){
+            printf("You are eligible for voting");
+        }else if(age>=18 && hasID==0){
+            printf("You meet the age requirment but cannot vote without ID");
+        }
+    }else
+         printf("You are not eligible for voting");
+
+}
